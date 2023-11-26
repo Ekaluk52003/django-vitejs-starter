8bbd2f462e315ba80faa36d9959fa21e85fa2d18
Ekaluk52003 <ekaluk.p@gmail.com>
2023-11-26 15:48:35 +0700

    redy for mass

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-
+import { Button } from "@/components/ui/button"
 
 function App() {
 
@@ -12,10 +12,9 @@ function App() {
         The Joke Tax Chronicles
       </h1>
       <p className='leading-7 [&:not(:first-child)]:mt-6'>
-        Once upon a time, in a far-off land, there was a very lazy king who
-        spent all day lounging on his throne. One day, his advisors came to him
-        with a problem: the kingdom was running out of money.
+       Intregate with dark mode. Thanks for shadCn
       </p>
+      <Button>Get start</Button>
     </section>
 
 
