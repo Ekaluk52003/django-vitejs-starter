a58997a465dd90a6ae392d2477fd317bb5354c63
Ekaluk52003 <ekaluk.p@gmail.com>
2023-11-26 14:18:20 +0700

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+const cssFilename = 'index.min.css'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  publicDir:'./public',
+  build:{
+    rollupOptions:{
+      output:{
+        assetFileNames:(file)=>{
+          return `assets/css/${cssFilename}`
+        },
+        entryFileNames:(file)=>{
+          return `assets/js/[name].min.js`
+        }
+      }
+    }
+  }
+})
